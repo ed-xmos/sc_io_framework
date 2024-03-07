e819d839f763b28aa35185d494184fb208077351
Ed <ed@xmos.com>
2024-03-07 17:33:21 +0000

    Set length of neopixel correctly

--- app_sc_framework/src/control_task.c
+++ app_sc_framework/src/control_task.c
@@ -14,27 +14,21 @@ void control_task(void){
     port_out(WIFI_WIRQ, 1); // Drive 3.3V to this pin
     set_pad_properties(WIFI_WIRQ, DRIVE_12MA, PULL_NONE, 0, 0);
 
-
     xclock_t cb = XS1_CLKBLK_3;
     clock_enable(cb);
     port_t p_neopixel = WIFI_MOSI;
     port_enable(p_neopixel);
     port_start_buffered(p_neopixel, 32);
     neopixel_state state;
-    unsigned length = 64;
-
+    unsigned length = 24;
 
     hwtimer_realloc_xc_timer();
     neopixel_init(&state, length, p_neopixel, cb, 3);
 
     while(1){
-
         for(unsigned i=0;i<length;i++){
             state.data[i] = i;
         }
-
-        while(!neopixel_drive_pins(&state, p_neopixel));
-
-        delay_milliseconds(50);
+        while(!neopixel_drive_pins(&state, p_neopixel)); // Takes about 1.2 ms for 24 neopixels
     }
 }
