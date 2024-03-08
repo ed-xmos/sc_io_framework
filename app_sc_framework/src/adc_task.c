398ce7d779b080f6455d9bb98277aa35f3772fcd
Ed <ed@xmos.com>
2024-03-08 15:45:29 +0000

    Refinement and filter

--- app_sc_framework/src/adc_task.c
+++ app_sc_framework/src/adc_task.c
@@ -12,9 +12,10 @@
 
 #include "app_main.h"
 
-#define ADC_READ_INTERVAL   (100 * XS1_TIMER_KHZ)    // Time in between individual conversions in milliseconds
+#define ADC_READ_INTERVAL       (1 * XS1_TIMER_KHZ)   // Time in between individual conversions
+#define ADC_BITS_TARGET         10                     // Resolution for conversion. 10 bits is practical maximum 
+#define RESULT_HISTORY_DEPTH    32                     // For filtering raw conversion values
 
-#define ABS(x) ((x)<0 ? (-x) : (x))
 
 static enum adc_state{
         ADC_IDLE = 2,
@@ -23,21 +24,54 @@ static enum adc_state{
 }adc_state;
 
 
-int post_process_result(int discharge_elapsed_time, int *zero_offset_ticks, int max_offsetted_conversion_time[], unsigned idx){
-    int zero_offsetted_ticks = discharge_elapsed_time - *zero_offset_ticks;
+static inline int post_process_result(  int discharge_elapsed_time,
+                                        int *zero_offset_ticks,
+                                        int max_offsetted_conversion_time[],
+                                        int *conversion_history,
+                                        unsigned adc_idx,
+                                        unsigned num_ports){
+    const int max_result_scale = (1 << ADC_BITS_TARGET) - 1;
+
+    // Apply filter. First write to history
+    static unsigned filter_write_idx = 0;
+    static unsigned filter_stable = 0;
+    unsigned offset = adc_idx * RESULT_HISTORY_DEPTH + filter_write_idx;
+    *(conversion_history + offset) = discharge_elapsed_time;
+    if(adc_idx == num_ports - 1){
+        if(++filter_write_idx == RESULT_HISTORY_DEPTH){
+            filter_write_idx = 0;
+            filter_stable = 1;
+        }
+    }
+    // Moving average filter
+    int accum = 0;
+    int *hist_ptr = conversion_history + adc_idx * RESULT_HISTORY_DEPTH;
+    for(int i = 0; i < RESULT_HISTORY_DEPTH; i++){
+        accum += *hist_ptr;
+        hist_ptr++;
+    }
+    int filtered_elapsed_time = accum / RESULT_HISTORY_DEPTH;
+    
+    // Remove zero offset and clip
+    int zero_offsetted_ticks = filtered_elapsed_time - *zero_offset_ticks;
     if(zero_offsetted_ticks < 0){
-        printstr("clip negative: "); printintln(zero_offsetted_ticks);
-        *zero_offset_ticks += (zero_offsetted_ticks / 2); // Move zero offset halfway to compensate
+        // printstr("clip negative: "); printintln(zero_offsetted_ticks);
+        if(filter_stable){
+            *zero_offset_ticks += (zero_offsetted_ticks / 2); // Move zero offset halfway to compensate
+        }
         zero_offsetted_ticks = 0;
     }
-    int pc_scaled_result = (100 * zero_offsetted_ticks) / max_offsetted_conversion_time[idx];
 
-    if(pc_scaled_result > 100){
+    // Calculate scaled output
+    int pc_scaled_result = (max_result_scale * zero_offsetted_ticks) / max_offsetted_conversion_time[adc_idx];
+
+    // Clip positive and move max if needed
+    if(pc_scaled_result > max_result_scale){
         // Handle moving up the maximum val
-        int new_max_offsetted_conversion_time = (100 * zero_offsetted_ticks) / pc_scaled_result;
-        printstr("clip positive: "); printintln(max_offsetted_conversion_time[idx]); printintln(new_max_offsetted_conversion_time);
-        max_offsetted_conversion_time[idx] += (new_max_offsetted_conversion_time - max_offsetted_conversion_time[idx]) / 2; // Move max halfway to compensate
-        pc_scaled_result = 100;
+        int new_max_offsetted_conversion_time = (max_result_scale * zero_offsetted_ticks) / pc_scaled_result;
+        // printstr("clip positive: "); printintln(max_offsetted_conversion_time[adc_idx]); printintln(new_max_offsetted_conversion_time);
+        max_offsetted_conversion_time[adc_idx] += (new_max_offsetted_conversion_time - max_offsetted_conversion_time[adc_idx]) / 2; // Move max halfway to compensate
+        pc_scaled_result = max_result_scale;
     }
 
     return pc_scaled_result;
@@ -51,33 +85,39 @@ void adc_task(void){
     unsigned p_adc_idx = 0;
     const unsigned num_ports = sizeof(p_adc) / sizeof(port_t);
 
-    unsigned results_pc[num_ports] = {0}; // The ADC read values in percent
+    int conversion_history[num_ports][RESULT_HISTORY_DEPTH] = {{0}};
 
-    const int port_drive = DRIVE_4MA;
+    unsigned results[num_ports] = {0}; // The ADC read values scaled to max_result_scale
+
+    const int port_drive = DRIVE_8MA;
     const int capacitor_nf = 10;
-    const int resistor_ohms_max = 9000;
+    const int resistor_ohms_max = 9500; // nominal minimum value
 
     const int charge_resistance_max = resistor_ohms_max / num_ports;
-    const int rc_times_to_charge = 10;
-    const int max_charge_period_ticks = (rc_times_to_charge * capacitor_nf * charge_resistance_max) / 10;
+    const int rc_times_to_charge_fully = 20;
+    const int max_charge_period_ticks = (rc_times_to_charge_fully * capacitor_nf * charge_resistance_max) / 10;
 
     const int max_discharge_period_ticks = (capacitor_nf * resistor_ohms_max) / 10;
-    xassert(ADC_READ_INTERVAL > 2 * max_discharge_period_ticks); // *2 to allow post processing
+    xassert(ADC_READ_INTERVAL > 2 * max_discharge_period_ticks); // *2 to allow post processing time
 
     // Calaculate zero offset based on drive strength
     int zero_offset_ticks = 0;
     switch(port_drive){
         case DRIVE_2MA:
             zero_offset_ticks = capacitor_nf * 24;
+            printstrln("DRIVE_2MA\n");
         break;
         case DRIVE_4MA:
-            zero_offset_ticks = capacitor_nf * 13;
+            zero_offset_ticks = capacitor_nf * 12;
+            printstrln("DRIVE_4MA\n");
         break;
         case DRIVE_8MA:
             zero_offset_ticks = capacitor_nf * 8;
+            printstrln("DRIVE_8MA\n");
         break;
         case DRIVE_12MA:
             zero_offset_ticks = capacitor_nf * 6;
+            printstrln("DRIVE_12MA\n");
         break;
     }
 
@@ -154,13 +194,18 @@ void adc_task(void){
                 int discharge_elapsed_time = discharge_end_time - discharge_start_time ;
                 port_clear_trigger_in(p_adc[p_adc_idx_other]);
 
-                results_pc[p_adc_idx] = post_process_result(discharge_elapsed_time, &zero_offset_ticks, max_offsetted_conversion_time, p_adc_idx);
+                results[p_adc_idx] = post_process_result(discharge_elapsed_time,
+                                                        &zero_offset_ticks,
+                                                        max_offsetted_conversion_time,
+                                                        (int*)conversion_history,
+                                                        p_adc_idx,
+                                                        num_ports);
 
                 hwtimer_set_trigger_time(t_adc, trigger_time);
                 adc_state = ADC_IDLE;
                 if(++p_adc_idx == num_ports){
                     p_adc_idx = 0;
-                    printf("ADCs: %d %d\n", results_pc[0], results_pc[1]);
+                    printf("ADCs: %d %d\n", results[0], results[1]);
                 }
             }
             break;
