37be2f1842817ff85e1dda3bb89d6785a79fce74
Ed <ed@xmos.com>
2024-03-08 12:34:00 +0000

    Updated multi-ADC

--- app_sc_framework/src/adc_task.c
+++ app_sc_framework/src/adc_task.c
@@ -10,8 +10,8 @@
 
 #include "app_main.h"
 
-#define ADC_READ_INTERVAL   (50 * XS1_TIMER_KHZ)    // Time in between individual conversions in milliseconds
-#define ADC_CHARGE_PERIOD   (100 * XS1_TIMER_MHZ)   // Time to charge cap in microseconds
+#define ADC_READ_INTERVAL   (10 * XS1_TIMER_KHZ)    // Time in between individual conversions in milliseconds
+#define ADC_CHARGE_PERIOD   (500 * XS1_TIMER_MHZ)   // Time to charge cap in microseconds
 #define ADC_NOISE_DEADBAND   2                      // change of 1/256 will not trigger  
 
 #define ABS(x) ((x)<0 ? (-x) : (x))
@@ -53,34 +53,58 @@ void adc_task(void){
     hwtimer_set_trigger_time(t_adc, trigger_time);
     triggerable_enable_trigger(t_adc);
 
+    // We read through different line
+    unsigned p_adc_idx_other = 0;
+
+
     printstrln("adc_task");
+
+    while(0){
+        port_out(p_adc[0], 1);
+        port_out(p_adc[1], 1);
+        delay_microseconds(500);
+        port_in(p_adc[0]);
+        port_out(p_adc[1], 0);
+        delay_milliseconds(2);
+
+        port_out(p_adc[0], 1);
+        port_out(p_adc[1], 1);
+        delay_microseconds(500);
+        port_in(p_adc[1]);
+        port_out(p_adc[0], 0);
+        delay_milliseconds(100);
+    }
 
     while(1){
         SELECT_RES(
             CASE_GUARD_THEN(t_adc, adc_state != ADC_CONVERTING, adc_timer_event),
-            CASE_GUARD_THEN(p_adc[p_adc_idx], adc_state == ADC_CONVERTING, adc_read)
+            CASE_GUARD_THEN(p_adc[p_adc_idx_other], adc_state == ADC_CONVERTING, adc_read)
         )
         {
             adc_timer_event:
             {
                 switch(adc_state){
                     case ADC_IDLE:
-                        port_out(p_adc[p_adc_idx], 1); // Drive a 1 to charge the capacitor
-                        /*     for(unsigned i = 0; i < num_ports; i++){
-                            port_out(p_adc[i], 1); // Drive a 1 to charge the capacitor
-                        } */
+                        for(unsigned i = 0; i < num_ports; i++){
+                            port_out(p_adc[i], 1); // Drive a 1 to charge the capacitor via all ines
+                        } 
                         trigger_time += ADC_CHARGE_PERIOD;
                         hwtimer_change_trigger_time(t_adc, trigger_time);
                         adc_state = ADC_CHARGING;
                     break;
 
                     case ADC_CHARGING:
-                        port_in(p_adc[p_adc_idx]); // Make input and start discharge
-                        discharge_start_time = hwtimer_get_time(t_adc);
                         hwtimer_clear_trigger_time(t_adc);
-                        port_set_trigger_in_equal(p_adc[p_adc_idx], 0); // Trigger ADC read when cap discharges to a read 0
+                        p_adc_idx_other = (p_adc_idx + 1) % num_ports; // We will read from a different line. Any will do.
+                        for(unsigned i = 0; i < num_ports; i++){
+                            port_in(p_adc[i]); // Make all inputs - hold charge for now
+                        } 
+                        port_set_trigger_in_equal(p_adc[p_adc_idx_other], 0); // Trigger ADC read when cap discharges to a read 0
                         trigger_time += ADC_READ_INTERVAL - ADC_CHARGE_PERIOD;
                         adc_state = ADC_CONVERTING;
+
+                        port_out(p_adc[p_adc_idx], 0); // Drive low to start discharge
+                        discharge_start_time = hwtimer_get_time(t_adc);
                     break;
 
                     default:
@@ -94,7 +118,7 @@ void adc_task(void){
             {
                 int discharge_end_time = hwtimer_get_time(t_adc);
                 int discharge_elapsed_time = discharge_end_time - discharge_start_time;
-                port_clear_trigger_in(p_adc[p_adc_idx]);
+                port_clear_trigger_in(p_adc[p_adc_idx_other]);
 
                 results[p_adc_idx] = discharge_elapsed_time;
 
