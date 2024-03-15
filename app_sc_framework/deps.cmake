5ffcaf1c731a1f6341c2d1e213ffdab46ee61345
Ed <ed@xmos.com>
2024-03-15 06:54:09 +0000

    modules and cmake_xcommon

---
+++ app_sc_framework/deps.cmake
@@ -0,0 +1,9 @@
+set(APP_DEPENDENT_MODULES   lib_i2c
+                            lib_i2s
+                            lib_uart
+                            lib_sw_pll
+                            lib_logging
+                            lib_xassert
+                            "lib_xcore_math(xcommon_cmake)"
+                            lib_xua
+                            )
