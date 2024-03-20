c607cbc9797e01f26929e8a2b121f304f82f8fb3
Ed <ed@xmos.com>
2024-03-20 11:41:30 +0000

    Get dep fetching working properly

--- app_sc_framework/deps.cmake
+++ app_sc_framework/deps.cmake
@@ -1,11 +1,49 @@
 set(APP_DEPENDENT_MODULES   lib_i2c
-                            lib_i2s
                             lib_qadc
-                            # lib_uart
-                            # lib_gpio
-                            lib_sw_pll
                             lib_logging
                             lib_xassert
                             "lib_xcore_math(xcommon_cmake)"
                             lib_xua
                             )
+
+include(FetchContent)
+
+if(NOT EXISTS ${XMOS_SANDBOX_DIR}/sw_usb_audio)
+    FetchContent_Declare(
+        sw_usb_audio
+        GIT_REPOSITORY git@github.com:xmos/sw_usb_audio
+        GIT_TAG develop
+        SOURCE_DIR ${XMOS_SANDBOX_DIR}/sw_usb_audio
+    )
+    FetchContent_Populate(sw_usb_audio)
+endif()
+
+if(NOT EXISTS ${XMOS_SANDBOX_DIR}/lib_uart)
+    FetchContent_Declare(
+        lib_uart
+        GIT_REPOSITORY git@github.com:xmos/lib_uart
+        GIT_TAG develop
+        SOURCE_DIR ${XMOS_SANDBOX_DIR}/lib_uart
+    )
+    FetchContent_Populate(lib_uart)
+endif()
+
+if(NOT EXISTS ${XMOS_SANDBOX_DIR}/lib_gpio)
+    FetchContent_Declare(
+        lib_gpio
+        GIT_REPOSITORY git@github.com:xmos/lib_gpio
+        GIT_TAG develop
+        SOURCE_DIR ${XMOS_SANDBOX_DIR}/lib_gpio
+    )
+    FetchContent_Populate(lib_gpio)
+endif()
+
+if(NOT EXISTS ${XMOS_SANDBOX_DIR}/lib_neopixel)
+    FetchContent_Declare(
+        lib_neopixel
+        GIT_REPOSITORY git@github.com:xmos/lib_neopixel
+        GIT_TAG develop
+        SOURCE_DIR ${XMOS_SANDBOX_DIR}/lib_neopixel
+    )
+    FetchContent_Populate(lib_neopixel)
+endif()
